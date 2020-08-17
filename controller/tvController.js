eb854a3dbae19897a90a1375ce539c33d2fd6022
jinnatul <morolswediu@gmail.com>
2020-08-17 22:50:16 +0600

    data filter country wise

--- controller/tvController.js
+++ controller/tvController.js
@@ -2,11 +2,15 @@ const catchAsync = require('../util/catchAsync');
 const tvModel = require('./../model/tvModel');
 
 exports.getIPTV = catchAsync(async (req, res, next) => {
-  let query = req.query.category, data;
-  data = await tvModel.find({ category: query });
-  if (data.length === 0) {
+  let category = req.query.category, data;
+  let country = req.query.country;
+
+  if (category) data = await tvModel.find({ category: category });
+  else if (country) data = await tvModel.find({ country: country });
+  
+  if (data === undefined || data === null || data.length === 0) 
     data = await tvModel.find();
-  }
+
   res.setHeader('Content-type', 'application/json');
   res.status(200).json(data);
 });
